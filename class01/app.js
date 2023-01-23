ec3247e0aae3f1c0120b323d8bc0b259e64382e6
Nathan Lima <nathanls.dev@gmail.com>
2023-01-23 14:29:14 -0300

    Let's Start

---
+++ class01/app.js
@@ -0,0 +1,15 @@
+let age = 28
+let currentYear = 2023
+console.log(age, currentYear)
+
+
+age = 29
+console.log(age)
+
+
+const points = 100
+console.log(points)
+
+
+var score = 75
+console.log(score)
