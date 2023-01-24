aff22725bf3a9328e539336f8f9785266a5c6944
Nathan Lima <nathanls.dev@gmail.com>
2023-01-24 13:14:25 -0300

    class 02-03 Strings

---
+++ class02/app.js
@@ -0,0 +1,27 @@
+// strings
+console.log(`hello, world`)
+
+const mail = `exemple@email.com`
+console.log(mail)
+
+// concatenation of strings
+const firstName = `Nathan`
+const lastName = `Drake`
+const fullName = firstName +' '+ lastName
+
+console.log(fullName)
+
+// accessing a character
+console.log(fullName[0])
+
+// string length
+console.log(fullName.length)
+
+// string methods
+console.log(fullName.toUpperCase())
+
+const result = fullName.toLocaleLowerCase()
+console.log(result)
+
+const index = mail.indexOf('@')
+console.log(index)
