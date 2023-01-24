2a9be96b60c890ebb3154507040ad2f7bdd5cafc
Nathan Lima <nathanls.dev@gmail.com>
2023-01-24 14:21:28 -0300

    class 02-04 String Methods

---
+++ class02/methodString.js
@@ -0,0 +1,8 @@
+// Common String Methods
+const email = `laracroft@email.com`
+
+// const lastIndexOfA = email.lastIndexOf('a')
+// const emailSlice = email.slice(0, 9)
+
+const emailReplace = email.replace('laracroft', 'tombraider')
+console.log(emailReplace, email)
