e7081c4e036cbb7399f4ff9016ced53f28968616
Nathan Lima <nathanls.dev@gmail.com>
2023-01-24 14:50:07 -0300

    class 02-05 Numbers

---
+++ class03/app.js
@@ -0,0 +1,34 @@
+// integer and decimals
+const radius = 10
+const pi = 3.14
+
+console.log(radius, pi)
+
+// arithmetic operators
+const area = pi * radius ** 2
+
+// operation order
+const crazyOperation = 5 * (10 - 3) ** 2
+
+console.log(crazyOperation)
+
+// 1° parentheses
+// 2° exponent, square
+// 3° multi, division
+// 4° addition, subtraction
+
+// increment and decrement
+let postLikes = 10
+
+// add, sub, multi, div
+postLikes /= 2
+
+console.log(postLikes)
+
+// NaN
+console.log(7 / 'oi')
+
+// concatenate String and Numbers
+const likesMessage = `O poste tem ${postLikes} likes.`
+
+console.log(likesMessage)
