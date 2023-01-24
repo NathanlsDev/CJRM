3d3cc9bfb6cf634600a0d754ea32931a6fc8670f
Nathan Lima <nathanls.dev@gmail.com>
2023-01-24 18:46:58 -0300

    class 03

--- class03/app.js
+++ class03/app.js
@@ -1,34 +1,14 @@
-// integer and decimals
-const radius = 10
-const pi = 3.14
+// Arrays
+let heroes = ['Batman', 'CatWoman', 'Iron Man']
+const ages = [31, 25, 39, 40, 25]
+const randomArray = ['Parker', 'Diana', 19, 18]
 
-console.log(radius, pi)
 
-// arithmetic operators
-const area = pi * radius ** 2
+// Array Methods
+const joinHeroes = heroes.join(', ')
+const indexOf25 = ages.indexOf(25)
+const moreHeroes = heroes.concat(['Super-Man', 'Wolverine'])
+const pushToHeroes = heroes.push('Cyclops', 'Hulk')
+const popHeroes = heroes.pop()
 
-// operation order
-const crazyOperation = 5 * (10 - 3) ** 2
-
-console.log(crazyOperation)
-
-// 1° parentheses
-// 2° exponent, square
-// 3° multi, division
-// 4° addition, subtraction
-
-// increment and decrement
-let postLikes = 10
-
-// add, sub, multi, div
-postLikes /= 2
-
-console.log(postLikes)
-
-// NaN
-console.log(7 / 'oi')
-
-// concatenate String and Numbers
-const likesMessage = `O poste tem ${postLikes} likes.`
-
-console.log(likesMessage)
+console.log(heroes)
