8ecec18c7412d41ccbcbf76274d33f3f00300924
Nathan Lima <nathanls.dev@gmail.com>
2023-01-25 17:07:48 -0300

    class 05

---
+++ class05/app.js
@@ -0,0 +1,18 @@
+// for loop
+/*
+const names = ['Linus', 'Ada', 'Bill']
+
+for (i = 0; i < names.length; i ++){
+  const HTMLTemplate = `<p>${names[i]}</p>`
+  console.log(HTMLTemplate)
+
+}
+*/
+
+const names = ['Jobs', 'Hopper', 'Turing']
+let i = 0
+
+while(i < names.length){
+  console.log(names[i])
+  i++
+}
