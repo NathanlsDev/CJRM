17dc75e95b85868c2082bd9ee5b78ca2bd0b8d33
Nathan Lima <nathanls.dev@gmail.com>
2023-01-28 18:51:11 -0300

    class 08

---
+++ class08/app.js
@@ -0,0 +1,37 @@
+// function declaration // hoisting priority
+function sayHi (){
+  console.log('hi')
+
+}
+
+// function expression
+const showFood = function (){
+  console.log('pizza')
+
+}
+
+sayHi()
+showFood()
+
+// arguments, parameters and default parameters
+const myFunc = function (name = 'Derpson', lastName = 'Derp'){
+  console.log(`Hi, ${name} ${lastName}!`)
+
+}
+
+myFunc()
+myFunc('Nathan', 'Lima')
+
+// return
+const double = function (number){
+  return number * 2 
+
+}
+
+const result = double(3)
+
+const showResult = function (value){
+  return `The result is: ${value}`
+}
+
+console.log(showResult(result))
