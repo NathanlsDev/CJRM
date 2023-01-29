491ed5d68cc8ff160bf9df3a5ac7771d660e6b05
Nathan Lima <nathanls.dev@gmail.com>
2023-01-29 19:09:38 -0300

    class 08-02

--- class08/app.js
+++ class08/app.js
@@ -1,37 +1,32 @@
 // function declaration // hoisting priority
-function sayHi (){
-  console.log('hi')
-
+function sayHi() {
+  console.log("hi");
 }
 
 // function expression
-const showFood = function (){
-  console.log('pizza')
-
-}
-
-sayHi()
-showFood()
+const showFood = function () {
+  console.log("pizza");
+};
+sayHi();
+showFood();
 
 // arguments, parameters and default parameters
-const myFunc = function (name = 'Derpson', lastName = 'Derp'){
-  console.log(`Hi, ${name} ${lastName}!`)
-
-}
-
-myFunc()
-myFunc('Nathan', 'Lima')
+const myFunc = function (name = "Derpson", lastName = "Derp") {
+  console.log(`Hi, ${name} ${lastName}!`);
+};
+myFunc();
+myFunc("Nathan", "Lima");
 
 // return
-const double = function (number){
-  return number * 2 
+// const double = function (number){
+//   return number * 2
+// }
 
-}
+// arrow function =>
+const double = number => number * 2;
+const result = double(3);
 
-const result = double(3)
-
-const showResult = function (value){
-  return `The result is: ${value}`
-}
-
-console.log(showResult(result))
+const showResult = function (value) {
+  return `The result is: ${value}`;
+};
+console.log(showResult(result));
