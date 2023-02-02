f533bcac05345af985e36ac4a3f52512dfff326a
Nathan Lima <nathanls.dev@gmail.com>
2023-02-01 23:26:11 -0300

    class 09

---
+++ class09/app.js
@@ -0,0 +1,23 @@
+// literal object
+let user = {
+  name: 'joão',
+  age: '31',
+  mail: 'example@email.com',
+  city: 'São Paulo',
+  blogPosts: ['Empadão de frango', '4 receitas com javascript e maisena']
+}
+
+console.log(user.blogPosts[1])
+
+user.age = 28
+
+console.log(user.age)
+
+const key = 'mail'
+console.log(user[key])
+
+user['name'] = 'José'
+
+console.log(user.name)
+
+console.log(typeof user)
