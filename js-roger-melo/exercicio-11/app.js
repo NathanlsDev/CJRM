1ac8422a1065c955fa49e8744b520fbf0db6dc1e
Nathan Lima <nathanls.dev@gmail.com>
2023-02-08 15:47:43 -0300

    exercise 11 - completed

--- js-roger-melo/exercicio-11/app.js
+++ js-roger-melo/exercicio-11/app.js
@@ -4,7 +4,7 @@
   - Declare uma constante que recebe o seu nome;
   - Ela deve ter um escopo global.
 */
-
+const name = 'Nathan'
 /*
   02
 
@@ -12,12 +12,18 @@
   - Crie uma função que ao ser invocada, exibe sua idade no console;
   - Dentro da função, declare uma let que armazena a sua idade;
   - Exiba sua idade no console, através da invocação da função;
-  - Do lado de fora da função exiba no console o valor da let que foi declarada  
-    dentro da função;
+  - Do lado de fora da função exiba no console o valor da let que foi declarada dentro da função;
   - Um erro com a mensagem "Uncaught ReferenceError: SUA_VARIÁVEL is not  
     defined" será exibido no console;
   - Você sabe por que isso aconteceu?
 */
+
+const age = (age) => {
+  let myAge = age
+  console.log(myAge)
+}
+// console.log(myAge)
+age(28)
 
 /*
   03
@@ -27,8 +33,7 @@
     - brand, que recebe a marca do carro;
     - colors, que recebe 3 cores para o carro;
     - isRunning, que recebe um boolean indicando se o carro está em movimento.  
-      Inicialmente, deve receber um boolean indicando que o carro não está em  
-      movimento;
+      Inicialmente, deve receber um boolean indicando que o carro não está em movimento;
     - run, que é um método que faz o carro andar e retorna a mensagem  
       "O NOME_DO_CARRO está em movimento";
     - stop, que é um método que faz o carro parar e retorna a mensagem  
@@ -37,11 +42,33 @@
       "O NOME_DO_CARRO está disponível nas cores COR_01, COR_02 e COR_03".
 */
 
+const car = {
+  name: 'XC40',
+  brand: 'Volvo',
+  colors: ['white', 'black', 'marine'],
+  isRunning: false,
+  run (){
+    this.isRunning = true
+    return `The ${this.name} is moving`
+  },
+  stop (){
+    this.isRunning = false
+    return `The ${this.name} stopped`
+  },
+  getColorsMessage (){
+    return `O ${this.name} está disponível nas cores ${this.colors.join(', ')}`
+  }
+}
+
 /*
   04
 
   - Faça o carro andar e exiba no console se ele realmente está em movimento.
 */
+
+console.log(car.run())
+console.log(car.isRunning)
+
 
 /*
   05
@@ -49,11 +76,16 @@
   - Faça o carro parar e exiba no console se ele realmente está parado.
 */
 
+console.log(car.stop())
+console.log(car.isRunning)
+
 /*
   06
 
   - Exiba, no console, a mensagem com as cores do carro.
 */
+
+console.log(car.getColorsMessage())
 
 /*
   07
@@ -61,3 +93,5 @@
   - Exiba, no console, a mensagem "O carro é um MARCA_DO_CARRO NOME_DO_CARRO";
   - Utilize a notação de colchetes para acessar as propriedades do carro.
 */
+
+console.log(`O carro é um ${car['brand']}`)
