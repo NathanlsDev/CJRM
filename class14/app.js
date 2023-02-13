3091a2811eb47cce59bd86b73e821848c4c13404
Nathan Lima <nathanls.dev@gmail.com>
2023-02-13 11:32:24 -0300

    Class 14

--- class14/app.js
+++ class14/app.js
@@ -1,2 +1,10 @@
 const article = document.querySelector('article')
-console.log(article.children)
+
+Array.from(article.children).forEach(element => {
+  element.classList.add('article__element')
+})
+
+
+const title = document.querySelector('h2')
+
+console.log(title.previousElementSibling)
