0e3c8425ff62db6b44b3d4a53053d3f7622628da
Nathan Lima <nathanls.dev@gmail.com>
2023-02-15 14:06:15 -0300

    class 15 append/prepend

--- class15/app.js
+++ class15/app.js
@@ -1,14 +1,22 @@
-const button = document.querySelector('button')
-button.addEventListener('click', () => {
-  console.log('clic')
-  
-})
+const ul = document.querySelector('ul')
+
+//ul.remove()
 
 const lis = document.querySelectorAll('li')
 lis.forEach(li => {
   li.addEventListener('click', event => {
     const clickedElement = event.target
+    clickedElement.remove()
 
     clickedElement.style.textDecoration = 'line-through'
   })
+})
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => {
+  const li = document.createElement('li')
+
+  li.textContent = 'New Item'
+  ul.prepend(li)
+  console.log(li)  
 })
