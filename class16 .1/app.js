5aeb16a2fb64930531f584086cb0d86aef1252ef
Nathan Lima <nathanls.dev@gmail.com>
2023-02-18 14:30:44 -0300

    class 16.1 - .some() method

---
+++ class16 .1/app.js
@@ -0,0 +1,21 @@
+const button = document.querySelector('button')
+const popup = document.querySelector('.popup-wrapper')
+
+
+
+button.addEventListener('click', () => {
+  popup.style.display = 'block'
+})
+
+
+
+popup.addEventListener('click', event => {
+  const classNameOfElement =  event.target.classList[0]
+  const classNames = ['popup-close', 'popup-wrapper', 'popup-link']
+  const shouldClosePopup = classNames.some(className =>
+    className === classNameOfElement)
+  
+  if(shouldClosePopup){
+    popup.style.display = 'none'
+  }
+})
