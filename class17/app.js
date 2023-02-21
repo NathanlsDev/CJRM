0ee992a2d618ec4b408a5ba0294fd25ace797022
Nathan Lima <nathanls.dev@gmail.com>
2023-02-21 14:42:15 -0300

    class 17 - regex

--- class17/app.js
+++ class17/app.js
@@ -5,3 +5,16 @@ form.addEventListener('submit', event => {
   event.preventDefault()
   console.log(event.target.username.value)
 })
+
+const username = 'Nathan'
+const pattern = /^[a-zA-Z]{3,}$/
+const isAMatch = pattern.test(username)
+
+if (isAMatch){
+  console.log('O teste passou =)')  
+} else {
+  console.log('O teste NÃO passou =(')
+}
+
+// const result = username.search(pattern)
+// console.log(result)
