8c5922816f3555b0ff96555956aa6867e71a2b6f
Nathan Lima <nathanls.dev@gmail.com>
2023-03-13 20:13:13 -0300

    class 19.2 - setInterval

--- class19/quiz-interativo/app.js
+++ class19/quiz-interativo/app.js
@@ -1,25 +1,38 @@
-const form = document.querySelector('.quiz-form')
-const finalResult = document.querySelector('.result')
+const form = document.querySelector(".quiz-form");
+const finalResult = document.querySelector(".result");
 
-const correctAnswers = ['B','B','B','B']
+const correctAnswers = ["B", "B", "B", "B"];
 
-form.addEventListener('submit', event => {
-  event.preventDefault()
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-  let score = 0
+  let score = 0;
   const userAnswers = [
     form.inputQuestion1.value,
     form.inputQuestion2.value,
     form.inputQuestion3.value,
     form.inputQuestion4.value,
-  ]
+  ];
 
-  userAnswers.forEach( (userAnswer, index) => {
-    if (userAnswer === correctAnswers[index]){
-      score += 25
+  userAnswers.forEach((userAnswer, index) => {
+    if (userAnswer === correctAnswers[index]) {
+      score += 25;
     }
-  })
+  });
+
+  scrollTo(0, 0)
+
   
-  finalResult.querySelector('span').textContent = `${score}%`
-  finalResult.classList.remove('d-none')
-})
+  finalResult.classList.remove("d-none");
+  
+  let counter = 0
+
+  const timer = setInterval(() => {
+    if(counter === score){
+      clearInterval(timer)
+    } 
+    finalResult.querySelector("span").textContent = `${counter}%`;
+    counter++
+  }, 10)
+});
+
