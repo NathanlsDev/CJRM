2796476a5db888f2b5cf16ce3c93d8f1c0662737
Nathan Lima <nathanls.dev@gmail.com>
2023-09-13 12:39:26 -0300

    exercise 22 - using chained methods

--- js-roger-melo/exercicio-22/app.js
+++ js-roger-melo/exercicio-22/app.js
@@ -6,8 +6,12 @@
   - Exiba o array ordenado no console.
 */
 
-const names = ['Caio', 'André', 'Dário']
+const getArrayCopy = (array) => array.map((item) => item);
 
+const names = ["Caio", "André", "Dário"];
+const orderedNames = getArrayCopy(names).sort();
+
+console.log(orderedNames);
 /*
   02
 
@@ -17,11 +21,16 @@ const names = ['Caio', 'André', 'Dário']
 */
 
 const characters = [
-  { id: 03, name: 'Simba' },
-  { id: 02, name: 'Nala' },
-  { id: 01, name: 'Scar' },
-  { id: 04, name: 'Mufasa' }
-]
+  { id: 3, name: "Simba" },
+  { id: 2, name: "Nala" },
+  { id: 1, name: "Scar" },
+  { id: 4, name: "Mufasa" },
+];
+
+const orderedCharacters = getArrayCopy(characters)
+  .sort((idValue1, idValue2) => idValue1.id - idValue2.id);
+
+console.log(orderedCharacters);
 
 /*
   03
@@ -31,7 +40,11 @@ const characters = [
   - Exiba o array ordenado no console.
 */
 
-const numbers = [41, 15, 63, 349, 25, 22, 143, 64, 59, 291]
+const numbers = [41, 15, 63, 349, 25, 22, 143, 64, 59, 291];
+
+const orderedNumbers = getArrayCopy(numbers)
+  .sort((number1, number2) => number1 - number2);
+console.log(orderedNumbers);
 
 /*
   04
@@ -39,7 +52,8 @@ const numbers = [41, 15, 63, 349, 25, 22, 143, 64, 59, 291]
   - Encontre e exiba no console o 1º item maior que 50 do array abaixo.
 */
 
-const randomNumbers = [10, 5, 0, 40, 60, 10, 20, 70]
+const randomNumbers = [10, 5, 0, 40, 60, 10, 20, 70];
+console.log(randomNumbers.find((number) => number > 50));
 
 /*
   05
@@ -49,7 +63,13 @@ const randomNumbers = [10, 5, 0, 40, 60, 10, 20, 70]
   - Exiba o array ordenado no console.
 */
 
-const people = ['Cauã', 'Alfredo', 'Bruno']
+const people = ["Cauã", "Alfredo", "Bruno"];
+
+const orderedPeople = getArrayCopy(people)
+  .sort()
+  .reverse();
+
+console.log(orderedPeople);
 
 /*
   06
@@ -59,7 +79,20 @@ const people = ['Cauã', 'Alfredo', 'Bruno']
   - Exiba a string no console.
 */
 
-const ingredients = ['vinho', 'tomate', 'cebola', 'cogumelo']
+const ingredients = ["vinho", "tomate", "cebola", "cogumelo"];
+
+const ingredientsMessage = ingredients.reduce(
+  (acc, ingredient, index, array) => {
+    const correctWordGender = /a$/.test(ingredient) ? "cozida" : "cozido";
+    const isLastItem = index === array.length - 1
+
+    const ingredientMessage = `${acc} ${ingredient} ${correctWordGender}`
+
+    return isLastItem
+      ? `${ingredientMessage}.` 
+      : `${ingredientMessage}, `
+  }, "");
+console.log(ingredientsMessage);
 
 /*
   07
@@ -69,17 +102,47 @@ const ingredients = ['vinho', 'tomate', 'cebola', 'cogumelo']
 */
 
 const topBrazilmovies = [
-  { title: 'Vingadores: Ultimato', peopleAmount: 19686119, distributedBy: 'Disney' },
-  { title: 'Titanic', peopleAmount: 17050000, distributedBy: 'Paramount / 20th Century' },
-  { title: 'O Rei Leão', peopleAmount: 16267649, distributedBy: 'Disney' },
-  { title: 'Vingadores: Guerra Infinita', peopleAmount: 14572181, distributedBy: 'Disney' },
-  { title: 'Tubarão', peopleAmount: 13035000, distributedBy: 'Universal' },
-  { title: 'Nada a Perder', peopleAmount: 11944985, distributedBy: 'Paris Filmes' },
-  { title: 'Os Dez Mandamentos', peopleAmount: 11259536, distributedBy: 'Paris / Downtown Filmes' },
-  { title: 'Tropa de Elite 2', peopleAmount: 11204815, distributedBy: 'Zazen' },
-  { title: 'Os Vingadores', peopleAmount: 10968065, distributedBy: 'Disney' },
-  { title: 'Dona Flor e Seus Dois Maridos', peopleAmount: 10735524, distributedBy: 'Embrafilme' }
-]
+  {
+    title: "Vingadores: Ultimato",
+    peopleAmount: 19686119,
+    distributedBy: "Disney",
+  },
+  {
+    title: "Titanic",
+    peopleAmount: 17050000,
+    distributedBy: "Paramount / 20th Century",
+  },
+  { title: "O Rei Leão", peopleAmount: 16267649, distributedBy: "Disney" },
+  {
+    title: "Vingadores: Guerra Infinita",
+    peopleAmount: 14572181,
+    distributedBy: "Disney",
+  },
+  { title: "Tubarão", peopleAmount: 13035000, distributedBy: "Universal" },
+  {
+    title: "Nada a Perder",
+    peopleAmount: 11944985,
+    distributedBy: "Paris Filmes",
+  },
+  {
+    title: "Os Dez Mandamentos",
+    peopleAmount: 11259536,
+    distributedBy: "Paris / Downtown Filmes",
+  },
+  { title: "Tropa de Elite 2", peopleAmount: 11204815, distributedBy: "Zazen" },
+  { title: "Os Vingadores", peopleAmount: 10968065, distributedBy: "Disney" },
+  {
+    title: "Dona Flor e Seus Dois Maridos",
+    peopleAmount: 10735524,
+    distributedBy: "Embrafilme",
+  },
+];
+
+const totalDisnetViews = topBrazilmovies
+  .filter(({ distributedBy }) => distributedBy === "Disney")
+  .reduce((acc, { peopleAmount }) => acc + peopleAmount, 0);
+
+console.log(totalDisnetViews);
 
 /*
   08
@@ -93,13 +156,19 @@ const topBrazilmovies = [
 */
 
 const pets = [
-  { name: 'Boris', age: 4, gender: 'Male', type: 'Dog' },
-  { name: 'Jimmy', age: 1, gender: 'Male', type: 'Cat' },
-  { name: 'Pérola', age: 2, gender: 'Female', type: 'Dog' },
-  { name: 'Lucy', age: 5, gender: 'Female', type: 'Cat' },
-  { name: 'Cristal', age: 3, gender: 'Female', type: 'Dog' },
-  { name: 'Chico', age: 6, gender: 'Male', type: 'Dog' }
-]
+  { name: "Boris", age: 4, gender: "Male", type: "Dog" },
+  { name: "Jimmy", age: 1, gender: "Male", type: "Cat" },
+  { name: "Pérola", age: 2, gender: "Female", type: "Dog" },
+  { name: "Lucy", age: 5, gender: "Female", type: "Cat" },
+  { name: "Cristal", age: 3, gender: "Female", type: "Dog" },
+  { name: "Chico", age: 6, gender: "Male", type: "Dog" },
+];
+
+const dogsInHumanAge = pets
+  .filter(({ type }) => type === "Dog")
+  .map(({ name, age, gender, type }) => ({ name, age: age * 7, gender, type }));
+
+console.log(dogsInHumanAge);
 
 /*
   09
@@ -107,6 +176,13 @@ const pets = [
   - Considerando o array topBrazilmovies, através do map ou do reduce, insira 
     os nomes dos filmes na ul do index.html.
 */
+const ul = document.querySelector(".list-group");
+
+const insertMovies = topBrazilmovies
+.reduce( (acc, { title }) => `${acc} <li>${title}</li>`
+, "");
+
+ul.innerHTML = insertMovies;
 
 /*
   10
