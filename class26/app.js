673ed2bcc61af4faf212143472d44b7500b18048
Nathan Lima <nathanls.dev@gmail.com>
2023-10-09 21:34:02 -0300

    class 27 - Fetch API

---
+++ class26/app.js
@@ -0,0 +1,7 @@
+fetch("http://jsonplaceholder.typicode.com/users")
+  .then(response => {    
+    console.log("Response: ", response)
+    return response.json()
+  })
+  .then(users => console.log(users))
+  .catch(error => console.log("Error: ", error));
