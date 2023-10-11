82c813779a61cf2daea2afbf55e682be9cdbd817
Nathan Lima <nathanls.dev@gmail.com>
2023-10-10 21:27:01 -0300

    class 28 - async/await

--- class26/app.js
+++ class26/app.js
@@ -1,7 +1,26 @@
-fetch("http://jsonplaceholder.typicode.com/users")
-  .then(response => {    
-    console.log("Response: ", response)
-    return response.json()
-  })
-  .then(users => console.log(users))
-  .catch(error => console.log("Error: ", error));
+const getUsers = async () => {
+  const response = await fetch("http://jsonplaceholder.typicode.com/users");
+  return await response.json()  
+};
+
+const logUsers =   async () => {
+  const users  = await getUsers()
+  console.log(users)
+}
+
+console.log(1)
+console.log(2)
+
+logUsers()
+
+console.log(3)
+console.log(4)
+
+
+// fetch("http://jsonplaceholder.typicode.com/users")
+//   .then(response => {
+//     console.log("Response: ", response)
+//     return response.json()
+//   })
+//   .then(users => console.log(users))
+//   .catch(error => console.log("Error: ", error));
