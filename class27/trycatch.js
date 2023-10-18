2245243d48b780640635f5576cc207f7d3833622
Nathan Lima <nathanls.dev@gmail.com>
2023-10-17 21:53:36 -0300

    class 27 - try catch

---
+++ class27/trycatch.js
@@ -0,0 +1,12 @@
+try {
+  console.log(oi);
+} catch (error) {
+  if (
+    error.name === "ReferenceError" &&
+    error.message === "oi is not defined"
+  ) {
+    const oi = "const oi criada";
+    console.log(oi);
+  }
+}
+console.log("oi");
